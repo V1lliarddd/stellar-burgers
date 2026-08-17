94a200dc396a787ad758a0b5393ba65f0be91dd9
Daniil Kamaev <V1lliard@yandex.ru>
2026-08-17 22:34:52 +0300

    доделал app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -14,19 +14,44 @@ import styles from './app.module.css';
 
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 import { Preloader } from '@ui';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { ProtectedRoute } from '../protected-route/protected-route';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchIngridients } from '../../services/slices/ingridients-slice';
+import { fetchUser } from '../../services/slices/user-slice';
+import { AppDispatch } from '../../services/store';
+import { RootState } from 'src/services/root-reducer';
 
 const App = () => {
-  /** TODO: взять переменные из стора */
-  const isIngredientsLoading = false;
-  const ingredients = [];
-  const error = null;
+  const dispatch = useDispatch<AppDispatch>();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const ingridientsState = useSelector((state: RootState) => state.ingredients);
+
+  const ingridients = ingridientsState.data;
+  const isIngridientsLoading = ingridientsState.isLoading;
+  const isIngridientsError = ingridientsState.error;
+
+  const userState = useSelector((state: RootState) => state.user);
+
+  const isUserAuthenticated = userState.user;
+  const isUserChecked = userState.checked;
+
+  const background = location.state?.background;
+
+  useEffect(() => {
+    dispatch(fetchIngridients());
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  const handleCloseModalWindow = () => navigate(-1);
 
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Routes>
+      <Routes location={background || location}>
         <Route path='/' element={<ConstructorPage />} />
         <Route path='/feed' element={<Feed />} />
         <Route
@@ -79,33 +104,42 @@ const App = () => {
         />
         <Route path='*' element={<NotFound404 />} />
       </Routes>
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='Детали заказа' onClose={handleCloseModalWindow}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal
+                title='Детали ингридиента'
+                onClose={handleCloseModalWindow}
+              >
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <ProtectedRoute>
+                <Modal title='Детали заказа' onClose={handleCloseModalWindow}>
+                  <OrderInfo />
+                </Modal>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      )}
 
-      <Routes>
-        <Route
-          path='/feed/:number'
-          element={
-            <Modal title='Детали заказа' onClose={() => {}}>
-              <OrderInfo />
-            </Modal>
-          }
-        />
-        <Route
-          path='/ingredients/:id'
-          element={
-            <Modal title='Детали ингридиента' onClose={() => {}}>
-              <IngredientDetails />
-            </Modal>
-          }
-        />
-        <Route
-          path='/profile/orders/:number'
-          element={
-            <Modal title='Детали заказа' onClose={() => {}}>
-              <OrderInfo />
-            </Modal>
-          }
-        />
-      </Routes>
+      {isIngridientsLoading && <Preloader />}
+      {isIngridientsError && <div>{isIngridientsError}</div>}
     </div>
   );
 };
